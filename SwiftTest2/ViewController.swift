9b39b8e0fa04eece5b058bca79b0befc52da6ea3
KaZ1124 <HiraKazu1124@gmail.com>
2018-03-23 05:59:22 +0900

    Initial Commit

---
+++ SwiftTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTest2
+//
+//  Created by 平川一樹 on 2018/03/23.
+//  Copyright © 2018年 平川 一樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
